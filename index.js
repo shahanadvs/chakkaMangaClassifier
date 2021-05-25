47848737525afcbdef79f9d0eec3d974b99e6925
Shahanad Vs <shahanadvs@gmail.com>
2021-05-25 13:17:52 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let app = express();
-let port = 8080;
-
-app.use(express.static("./src"));
-
-app.listen(port, function () {
-  console.log(`Listening at http://localhost:${port}`);
-});
